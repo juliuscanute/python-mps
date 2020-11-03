94de58de01277feeb4fb7c858a60e045148b96eb
Julius Canute <juliuscanute@gmail.com>
2020-11-03 13:41:09 +1100

    Revert "update docker"
    This reverts commit a2c5225c38d7e9637bfacf23782e70b8515636e0.

--- actions/mps-docker-action/entrypoint.sh
+++ actions/mps-docker-action/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
-ant=$(/opt/ant/bin/ant -file /github/worspace/common/build.xml -Dartifacts.mps=/usr/local/mps/MPS\ 2020.2  assemble)
+ant=$(/opt/ant/bin/ant -buildfile /github/workspace/common/build.xml -Dartifacts.mps=/usr/local/mps/MPS\ 2020.2  assemble)
 echo "::set-output name=build-log::$ant"
