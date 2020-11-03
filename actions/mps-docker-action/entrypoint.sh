f61293b15f1b746596521366078f819be2e44fe4
Julius Canute <juliuscanute@gmail.com>
2020-11-03 12:44:32 +1100

    use relative path

--- actions/mps-docker-action/entrypoint.sh
+++ actions/mps-docker-action/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
-ant=$(/opt/ant/bin/ant -Dartifacts.mps=/usr/local/mps/MPS\ 2020.2  assemble)
+ant=$(/opt/ant/bin/ant -file ./common/build.xml -Dartifacts.mps=/usr/local/mps/MPS\ 2020.2  assemble)
 echo "::set-output name=build-log::$ant"
