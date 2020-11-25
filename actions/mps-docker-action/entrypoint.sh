ceff6ca4f8d0417da94713d6cd971bd9e7b2b9da
Julius Canute <juliuscanute@gmail.com>
2020-11-25 21:04:34 +1100

    update version in entrypoint

--- actions/mps-docker-action/entrypoint.sh
+++ actions/mps-docker-action/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
-ant=$(/opt/ant/bin/ant -buildfile /github/workspace/common/build.xml -Dartifacts.mps=/usr/local/mps/MPS\ 2020.2  assemble)
+ant=$(/opt/ant/bin/ant -buildfile /github/workspace/common/build.xml -Dartifacts.mps=/usr/local/mps/MPS\ 2020.1  assemble)
 echo "::set-output name=build-log::$ant"
